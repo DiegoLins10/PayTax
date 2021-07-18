e086ff0faf305ee184bbde52ae2c48965f48e119
diegolins10 <diegofernandeslins@gmail.com>
2021-07-18 20:07:27 -0300

    sub class company

---
+++ PayTax/Entities/Company.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayTax.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company(int numberOfEmployees, string name, double anualInCome) : base(name, anualInCome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            double tax;
+            if(NumberOfEmployees > 10)
+            {
+                tax = 0.14;
+            }
+            else
+            {
+                tax = 0.16;
+            }
+            return AnualInCome * tax;
+        }
+    }
+}
