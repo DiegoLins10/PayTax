416c815e76ac04a154c9a477ad88d6eef40a59f3
diegolins10 <diegofernandeslins@gmail.com>
2021-07-18 19:52:13 -0300

    super class

---
+++ PayTax/Entities/TaxPayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayTax.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualInCome { get; set; }
+
+        protected TaxPayer(string name, double anualInCome)
+        {
+            Name = name;
+            AnualInCome = anualInCome;
+        }
+
+        public abstract double Tax();
+    }
+}
