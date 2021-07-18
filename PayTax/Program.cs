f1b1bbe0fe14e878afa17d0548c2c6e3b7c52a18
diegolins10 <diegofernandeslins@gmail.com>
2021-07-18 19:47:52 -0300

    Add project files.

---
+++ PayTax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayTax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
